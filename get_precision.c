97b4995ca6f080cdaf4242be09bd154825e277e7
ilhamazalmad <ilhamazalmad11@gmail.com>
2023-04-25 08:23:30 -0700

    Handle the precision for non-custom conversion specifiers.

--- get_precision.c
+++ get_precision.c
@@ -22,13 +22,12 @@ int get_precision(const char *format, int *i, va_list list)
 	{
 		if (is_digit(format[curr_i]))
 		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
+			precision = precision * 10 + (format[curr_i] - '0');
 		}
 		else if (format[curr_i] == '*')
 		{
-			curr_i++;
 			precision = va_arg(list, int);
+			curr_i++;
 			break;
 		}
 		else
